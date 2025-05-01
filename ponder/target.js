18a8f748b7a13f40ddbffed14f333cf29acbb5e6
Will Giard <73901261+WillG2001@users.noreply.github.com>
2025-04-30 18:13:39 -0600

    added dark/light mode to mission statement

---
+++ ponder/target.js
@@ -0,0 +1,10 @@
+const introParagraph = document.querySelector('#intro');
+
+introParagraph.style.backgroundColor = '#f2c556';
+
+const emphasis = document.querySelector('em');
+
+emphasis.innerText = "USS Voyager Starship"
+
+let image = document.createElement('img');
+image.setAttribute('src', '')
