bd9acce760e06c0590c0dd29ab798d5ee1a6e6c8
Will Giard <73901261+WillG2001@users.noreply.github.com>
2025-05-17 17:20:55 -0600

    coolpics 2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -15,30 +15,25 @@ function handleResize() {
 }
 
 const gallery = document.querySelector('.gallery');
-const closeButton = document.querySelector('.close-viewer');
 const modal = document.createElement('dialog');
-modal.innerHTML = '<img><button class="close-viewer">X</button>';
+
 document.body.appendChild(modal);
 
 function displayModal (event) {
     var image = event.target.closest('img');
-    modal.img.src = image.src.split('-')[0] + '-full.jpeg';
+    const newImgSrc = image.src.split('-')[0] + '-full.jpeg';
+    modal.innerHTML = `<div class="modal-content"><img src="${newImgSrc}"><button class="close-viewer">X</button></div>`;
     modal.showModal();
+    const closeButton = modal.querySelector('.close-viewer');
+    closeButton.addEventListener("click", closeModal)
+    closeButton.onClick = closeModal;
 }
 
 function closeModal () {
     modal.close();
 }
 
-modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.close();
-  }
-})
-
-
 handleResize();
 window.addEventListener("resize", handleResize)
 menuButton.addEventListener("click", toggleMenu)
 gallery.addEventListener("click", displayModal)
-closeButton.addEventListener("click", closeModal)
