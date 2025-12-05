dea310f4de8fd18b4828d23fe9a500f37edef21c
Will Giard <73901261+WillG2001@users.noreply.github.com>
2025-12-05 15:24:48 -0700

    final site project

---
+++ reviewsite/site.js
@@ -0,0 +1,179 @@
+const reviews = [
+    {
+        title: "Donkey Kong Bananza (2025)",
+        image: "images/dkb.png",
+        category: "game",
+        imageAlt: "Donkey Kong Bananza box art",
+        reviewText: "Donkey Kong Bananza is the latest entry in the classic Donkey Kong franchise for the Nintendo Switch 2. And boy, is it a winner! Fans of Super Mario Odyssey will notice vast similarities between the two games. But Bananza is more than just Odyssey with a DK skin. It’s full of new mechanics and is truly a Donkey Kong game at heart. The main mechanic is that DK can punch and smash his way through just about any surface in the game. This leads to all sorts of fun exploration and secrets to find. It’s an absolute joy to find bananas, fossils, and other collectibles! Bananas lead to unlocking new skills for DK, so there’s a purpose to exploration. Combine that with some excellent platforming and a heartwarming story thanks to the inclusion of young Pauline, and you have a game that’s truly special. I honestly can’t find anything really wrong with it, and it’s hard to put it down! And I won’t spoil the ending sequence, but it’s some of the most fun I’ve had with a game in a long time.",
+        rating: "10"
+    },
+    {
+        title: "Mario Kart World (2025)",
+        image: "images/mkw.png",
+        category: "game",
+        imageAlt: "Mario Kart World box art",
+        reviewText: "Mario Kart World is the next entry in the Mario Kart series, released as a launch title for Nintendo’s new console, the Switch 2. I did a full review on my YouTube channel, but here is a summary of my thoughts. Overall, I really enjoy this game. I hesitate to say it’s the best Mario Kart game ever made, but after logging over 100 hours over the past several months, I can safely say it’s one of my personal favorites. It offers a lot of content with its open-world structure and “Free Roam” mode, and the individual tracks are really well-designed. The “intermission” tracks are more controversial due to their mostly straightforward nature, but I found that these offered a unique racing experience, different from the classic “3-lap” tracks. Online play is chaotic as ever and will test your skills, especially in the wonderful new “Knockout Tour” mode. It’s a great next step from the classic Mario Kart formula, and it makes me excited about the series’ future.",
+        rating: "8"
+    },
+    {
+        title: "Super Mario 64 (1996)",
+        image: "images/sm64.png",
+        category: "game",
+        imageAlt: "Super Mario 64 box art",
+        reviewText: "I recently beat Super Mario 64, a true Nintendo 64 classic, for the first time on Nintendo Classics on the Switch, after many years of playing it on the Wii’s Virtual Console. Super Mario 64 is the game that set the stage for many three-dimensional games to come afterward. It still holds up well today, although certain things are a bit outdated, such as the game’s camera and how Mario controls. But it’s an absolute joy to explore the painting worlds of Peach’s castle and hunt down Stars. In many ways, this game was the first of its kind, and spawned a whole genre of “collectathon” 3D platformers. And for that, this game will always have my respect. I hope to one day 100% complete this game by collecting all 120 Stars!",
+        rating: "8"
+    },
+    {
+        title: "Metroid Fusion (2002)",
+        image: "images/metroid-fusion.jpg",
+        category: "game",
+        imageAlt: "Metroid Fusion box art",
+        reviewText: "Originally released on the Game Boy Advance, Metroid Fusion is the fourth game in the mainline Metroid series, and it’s a personal favorite of mine. I recently replayed it on the Switch’s Nintendo Classics. The game follows Samus exploring the BSL Research Station in order to neutralize a threat to the galaxy known as the X Parasites. Whereas Super Metroid, the SNES prequel to this game, had a more open-ended structure, this game follows a more linear mission-based structure. However, there’s still a lot you need to figure out on your own, especially in the later parts of the game, so it still feels like a true Metroid experience. Finding upgrades for Samus to get stronger and access new areas as the game progresses still feels great, and there’s plenty of challenge to be had, especially with the game’s bosses. The atmosphere is great too, with a sense of fear permeating throughout the game as you’re hunted by your evil clone, the SA-X. I’d highly recommend this game to anyone who enjoys Metroidvanias and action games in general.",
+        rating: "8"
+    },
+    {
+        title: "Super Mario World 2: Yoshi’s Island (1995)",
+        image: "images/snes_yoshis_island.jpg",
+        category: "game",
+        imageAlt: "Yoshi's Island box art",
+        reviewText: "I recently beat Yoshi’s Island on the Nintendo Classics on the Switch. I’m a fan of the first Super Mario World, and always wanted to play this game. It’s less of a sequel to Super Mario World and more of its own thing, but that isn’t a bad thing! It’s still one of the more fun 2D platformers I’ve played. Yoshi is super fun to control, and making and throwing eggs is a great mechanic. The concept of having to carry around Baby Mario is a good one. I like how damage is handled in this game: when Yoshi takes a hit, Baby Mario goes into a bubble and you have to retrieve him before a timer expires. I also really like the game’s art style: everything looks like it could have been hand-drawn with crayons! It’s also a challenging game, especially if you try to go for a perfect 100 score on each level. Overall, this game is a classic and I’d recommend it to anyone that enjoys a challenging platformer and hasn’t played this one.",
+        rating: "8"
+    },
+    {
+        title: "Balatro (2024)",
+        image: "images/balatro.jpg",
+        category: "game",
+        imageAlt: "Balatro logo",
+        reviewText: "A poker rogue-like. It might sound like a weird idea on paper, but once you try Balatro, you won’t want to put it down! I played the Nintendo Switch version of Balatro and had a blast with it. It’s easy to pick up but difficult to master! The game revolves around you trying to construct the best poker hand you can to score points. You need a certain number of points in a set number of hands to beat the “blind” you’re up against and advance to the next one, and as you make your way through the antes, it gets more challenging. There are all sorts of special cards and Jokers you can obtain that make the game even more interesting. It’s so satisfying to have the right combination of special cards to rack up a ton of points! I’d recommend Balatro to anyone that enjoys card games and rogue-likes!",
+        rating: "9"
+    },
+    {
+        title: "Mario & Luigi: Brothership (2024)",
+        image: "images/m-and-l-bs.jpg",
+        category: "game",
+        imageAlt: "Mario & Luigi: Brothership box art",
+        reviewText: "Mario & Luigi: Brothership is the sixth entry in the beloved Mario & Luigi spinoff series of RPGs, released on Nintendo Switch. The game stars the iconic brothers exploring the new land of Concordia on a quest to discover and reconnect the scattered islands on the map. It has that charm we’ve come to love from the series, and a great battling system that was a highlight for me. Traveling the seas on Shipshape Island and discovering new islands is a great concept that works well for a Mario & Luigi game. The game definitely has some pacing issues, as it takes a while to really get started, but once it does, you’re in for a treat. It’s a long game that took me 50 hours to complete (granted, I did all of the sidequests as well), but as someone that enjoys games that I can’t complete in one sitting, and encourage me to take my time, the length wasn’t really a problem for me. It’s got great writing and humor as always, and a particular plot twist I won’t spoil that makes for one of the most unhinged endings to a Nintendo game I’ve seen, and perhaps the best Mario villain since Super Paper Mario. If you like longer games and are a fan of the series, you’ll probably like Brothership a lot.",
+        rating: "7"
+    },
+    {
+        title: "Stranger Things (2016-2025)",
+        image: "images/Stranger-Things-Poster.jpeg",
+        category: "show",
+        imageAlt: "Stranger Things poster",
+        reviewText: "Netflix’s Stranger Things became a cultural phenomenon the likes of which I haven’t really seen with most TV shows. Set in the 1980s, the show follows a group of middle-schoolers and high-schoolers as they investigate, well, strange things happening in their small town of Hawkins, Indiana. The disappearance of Will Byers begins an epic adventure full of creepy monsters, government conspiracies, and even the involvement of Russian scientists and armies. Packed with 80s pop-culture references and parallels to Dungeons & Dragons, and a great sense of mystery and suspense throughout the whole show, it’s no wonder the show became an instant classic, especially Season 1. It’s got great character development, and the characters play really well off of each other’s strengths and weaknesses. The designs of monsters such as the Demogorgon and the Mind Flayer, and the Upside Down (a dark parallel version of Hawkins) are really cool. The inclusion of Eleven, a girl with telekinetic powers, is fantastic, and she makes for a great hero of the story. I look forward to the conclusion of this amazing series with Season 5. If the show continues its path of greatness, the final battle between the heroes and the evil Vecna with his forces of the Upside Down will be legendary. If you haven’t watched Stranger Things, I would highly recommend it, as long as you don’t mind some elements of fear.",
+        rating: "9"
+    },
+    {
+        title: "Lost (2004-2010)",
+        image: "images/lost-poster.jpg",
+        category: "show",
+        imageAlt: "Lost poster",
+        reviewText: "ABC’s Lost is an interesting show. It starts out as a classic survival story when the passengers of Oceanic Flight 815 crash-land on a mysterious, uncharted island. However, the story quickly twists and turns with the inclusion of things such as a monster made of black smoke, the discovery of underground hatches built by a mysterious organization called the “Dharma Initiative”, and a group of island natives known as “The Others” that begin to terrorize and kidnap the Flight 815 passengers. The show raises a lot of questions almost every episode, and not all of them get answered. That can be frustrating at times, but it leaves a lot to your imagination, which is cool. The episodes also follow a really interesting structure with flashback scenes about a character’s life before the crash, interspersed with the happenings on the island. Overall, Lost is a very unique show that I’m glad I finally got to watch. While it can be confusing and hard to follow sometimes, it still has some of the best scenes in television I’ve seen, which is why I recommend it.",
+        rating: "7"
+    },
+    {
+        title: "Psych (2006-2014)",
+        image: "images/psych.jpg",
+        category: "show",
+        imageAlt: "Psych poster",
+        reviewText: "USA Network’s Psych is a hilarious show that I absolutely loved every minute of. I can’t believe I haven’t watched it until this year! It may have surpassed The Office as my favorite comedy show, which is saying a lot. The show follows an extremely observant guy named Shawn and his best friend Gus as they work cases for the Santa Barbara Police Department. The catch is an absurd one but it somehow works great: Shawn is pretending to be psychic, which leads to all sorts of crazy antics. The show’s humor is one of its greatest strengths: Shawn and Gus are a great comedic duo that play off of each other perfectly, and some of the scenarios they find themselves in are downright ridiculous and hilarious. But it’s the show’s heart that really hooked me, and the relationships between the characters are the driving force behind everything. In my opinion, it’s just about the perfect show. I wholeheartedly recommend it!",
+        rating: "10"
+    },
+    {
+        title: "Interstellar (2014)",
+        image: "images/Interstellar_film_poster.jpg",
+        category: "movie",
+        imageAlt: "Interstellar poster",
+        reviewText: "Christopher Nolan’s Interstellar is a special film. It follows a group of astronauts who embark on an epic mission to find a new home for Earth’s inhabitants who are suffering from famine and dust storms. The movie features spectacular cinematography with breathtaking scenes, especially during space travel and when the astronauts are exploring new planets. The narrative is emotionally compelling as well, with an emphasis on the family of Joseph Cooper, the main character (played by Matthew McConaughey). Top it off with excellent sound design, and a fantastic score by Hans Zimmer, and you have a cinematic masterpiece.",
+        rating: "10"
+    },
+    {
+        title: "La La Land (2016)",
+        image: "images/La_La_Land_(film).png",
+        category: "movie",
+        imageAlt: "La La Land poster",
+        reviewText: "I’m a fan of musicals, and La La Land is a great example of how to do a musical comedy-drama right. Starring Emma Stone and Ryan Gosling, the film follows an aspiring actress and a jazz pianist as they meet, fall in love, and pursue their dreams together. The screenplay is wonderful and quite entertaining. The music is very good as well, with great choreography to go along with it. One particular standout to me is the piano theme heard throughout the film that symbolizes Mia and Seb’s relationship. Overall, I really enjoyed La La Land, and if you like musicals, you probably will too!",
+        rating: "8"
+    },
+    {
+        title: "Superman (2025)",
+        image: "images/Superman_(2025_film)_poster.jpg",
+        category: "movie",
+        imageAlt: "Superman poster",
+        reviewText: "In a day and age where superhero movies are everywhere, James Gunn manages to deliver a reboot of a beloved superhero franchise that earnestly begins a new era of DC films and stands out in a sea of comic book movies. It features excellent performances by David Corenswet as Clark Kent/Superman, Nicholas Hoult as Lex Luthor, and others as well. The film follows Superman as he aims to win back public support after his intervention in an international conflict orchestrated by Lex Luthor, all the while, learning about his purpose and who he truly is. It’s a movie about the value of humanity and offers a refreshing take on a classic character. I was greatly surprised by it and would definitely recommend it to comic book fans and moviegoers alike!",
+        rating: "8"
+    },
+    {
+        title: "The Fantastic Four: First Steps (2025)",
+        image: "images/The_Fantastic_Four_First_Steps_poster.jpg",
+        category: "movie",
+        imageAlt: "The Fantastic Four: First Steps poster",
+        reviewText: "I was a huge fan of the Marvel Cinematic Universe during the Infinity Saga, and gradually began to lose interest in the overall MCU as time went on and quality of films and shows began to vary wildly. However, there have been some standout films in recent years, and The Fantastic Four: First Steps, the most recent MCU entry at the time of this review, is a solid example of one. It’s the second reboot of the Fantastic Four film series, and as someone that doesn’t have much attachment to the franchise, I found it quite enjoyable and a good return to form for the MCU. It follows the titular team led by Reed Richards (Pedro Pascal) as they fight to protect Earth from Galactus. It’s a strong narrative about the power of family and humanity, and features a unique retro-futuristic 1960s style. It was a fun time and I’d recommend it for anyone who’s been feeling disconnected from the MCU lately like me.",
+        rating: "7"
+    }
+];
+
+let reviewContainer = document.querySelector('#reviewSection');
+
+let button = document.querySelector('.searchBtn');
+
+button.addEventListener('click', search);
+
+document.querySelector('#category').addEventListener('change', search);
+
+function search() {
+    let reviewQuery = document.querySelector('#search').value;
+    let selectedCategory = document.querySelector('#category').value;
+
+    let filterReviews = reviews.filter(function (review) {
+        let matchesText =
+            review.title.toLowerCase().includes(reviewQuery.toLowerCase()) ||
+            review.reviewText.toLowerCase().includes(reviewQuery.toLowerCase());
+
+        let matchesCategory =
+            selectedCategory === 'all' || review.category === selectedCategory;
+
+        return matchesText && matchesCategory;
+    });
+
+    let sortedReviews = filterReviews.sort(compareReviews);
+
+    function compareReviews(a, b) {
+        if (a.title < b.title) {
+            return -1;
+        } else if (a.title > b.title) {
+            return 1;
+        }
+        return 0;
+    }
+
+    // clear out screen
+    reviewContainer.innerHTML = ''
+
+    // output on screen
+    sortedReviews.forEach(function (review) {
+        renderReview(review);
+    })
+}
+
+function reviewTemplate(review) {
+    return `
+    <div class="review">
+      <h2>${review.title}</h2>
+      <img src="${review.image}" alt="${review.imageAlt}" class="reviewImg">
+      <p>${review.reviewText}</p>
+      <h3>Rating: ${review.rating}</h3>
+    </div>
+  `
+}
+
+function renderReview(review) {
+    let html = reviewTemplate(review);
+    reviewContainer.innerHTML += html;
+}
+
+function init() {
+    reviews.forEach(r => { renderReview(r) });
+}
+
+init();
